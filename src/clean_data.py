78b2f5a168bb3226082f5cd79d5a50995c5e7c07
062n <nicholaszlee@uchicago.edu>
2024-03-01 14:31:11 -0600

    add functionality

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,7 +1,37 @@
+import pandas as pd
+import numpy as np
 
-
-def clean_data(range):
+def clean_data(period):
+    start, end = period
     
-    pass
+    df = pd.read_parquet("data/pulled/13f.parquet")
+    df = df[df['fdate'] <= end]
+    df = df.dropna(subset=['prc','shrout1'])
+    df = df[(df['stkcd']=='0') | (df['stkcd'].isnull())]
+    df = df[(df['exchcd'].isin(['A','B','V']))  | (df['exchcd'].isnull())]
 
-    return df
+    df_mf = pd.read_parquet("data/pulled/Mutual_Fund.parquet")
+    df['mf'] = df['mgrname'].isin(df_mf['mgrco'].drop_duplicates())
+
+    df_pf = pd.read_csv('data/manual/PF_names.csv')
+    df['pf'] = df['mgrname'].isin(df_pf['PF_name'])
+
+    last_type_before_dec98 = df[df['fdate'] < '1998-12-01'].groupby(['mgrno', 'mgrname'])['typecode'].last().rename('typecode_correct')
+    df = df.merge(last_type_before_dec98, on=['mgrno', 'mgrname'], how='left')
+    df.loc[df['fdate'] >= '1998-12-01', 'typecode'] = df['typecode_correct'].fillna(df['typecode'])
+
+    most_recent_type_code = df.groupby(['mgrno', 'mgrname'])['typecode'].last().rename('typecode_recent')
+    df = df.merge(most_recent_type_code, on=['mgrno', 'mgrname'])
+    df['typecode'] = df['typecode_recent']
+
+    df['new_typecode'] = np.nan
+    df.loc[df['typecode'] == 1, 'new_typecode'] = 1
+    df.loc[df['typecode'] == 2, 'new_typecode'] = 2
+    df.loc[df.groupby(['mgrno', 'mgrname'])['mf'].transform('any') & df['typecode'].isin([3,4,5]), 'new_typecode'] = 4
+    df.loc[df.groupby(['mgrno', 'mgrname'])['pf'].transform('any') & df['typecode'].isin([3,4,5]), 'new_typecode'] = 5
+    df.loc[df['typecode'] == 3, 'new_typecode'] = 3
+    df['new_typecode'] = df['new_typecode'].fillna(6)
+    df['typecode'] = df['new_typecode']
+
+    df = df[df['fdate'] >= start]
+    return df[['fdate', 'mgrno', 'mgrname', 'typecode', 'prc', 'shrout1']]
