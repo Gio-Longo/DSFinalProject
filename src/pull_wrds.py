965d8d7674e681d04dafce5ca5ab6ff1710e309e
dsbailey02 <145408855+dsbailey02@users.noreply.github.com>
2024-02-29 22:41:05 -0600

    Update pull_wrds.py

--- src/pull_wrds.py
+++ src/pull_wrds.py
@@ -12,7 +12,7 @@ DATA_DIR = Path(config.DATA_DIR)
 WRDS_USERNAME = config.WRDS_USERNAME
 
 
-def pull_13f(wrds_username=WRDS_USERNAME, start_date = '03/31/1980', end_date = '12/31/1984'):
+def pull_13f(wrds_username=WRDS_USERNAME, start_date = '03/31/1980', end_date = '12/31/2024'):
 
     my_params = {'start_date':start_date, 'end_date': end_date}
 
@@ -34,7 +34,7 @@ def pull_13f(wrds_username=WRDS_USERNAME, start_date = '03/31/1980', end_date = 
 
     return df_13f
 
-def pull_mf_mapping(wrds_username=WRDS_USERNAME, start_date = '03/31/1980', end_date = '12/31/1984'):
+def pull_mf_mapping(wrds_username=WRDS_USERNAME, start_date = '03/31/1980', end_date = '12/31/2024'):
 
     my_params = {'start_date':start_date, 'end_date': end_date}
 
