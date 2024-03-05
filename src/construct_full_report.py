7f8441ce66e97536ee1a52e208a1b4fc8a4a8335
dsbailey02 <145408855+dsbailey02@users.noreply.github.com>
2024-03-05 16:04:24 -0600

    Update construct_full_report.py

--- src/construct_full_report.py
+++ src/construct_full_report.py
@@ -35,11 +35,12 @@ def construct_full_report():
     dfs_old = build_DFs(df_old, periods_old)
     dfs_new = build_DFs(df_new, periods_new)
 
-    type_counts_df, aum_df, mgrs_df = construct_stats(df_old)
-    plot_stats_data(type_counts_df, 'Count', 'Institution Type Counts Over Time', 'type_counts.png'),
+    avg_df, aum_df, mgrs_df = construct_stats(df_old)
     plot_stats_data(aum_df, 'AUM', 'AUM Over Time', 'aum.png', True),
     plot_stats_data(mgrs_df, 'UniqueMgrCounts', 'Managers Over Time', 'mgrs.png')
+    plot_stats_data(avg_df, 'AUM', 'Average AUM Over Time', 'avg_aum.png'),
 
     md_path = Path("../README.md")
 
-    df_to_latex_with_md_and_plots(df_old, df_new, ['type_counts.png', 'aum.png', 'mgrs.png'], md_path, "full_report.tex")
+    df_to_latex_with_md_and_plots(df_old, df_new, ['aum.png', 'mgrs.png', 'avg_aum.png'], md_path, "full_report.tex")
+
